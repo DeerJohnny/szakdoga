98683a701929902aed19bf9d868f9a792f61aefe
Deer.Daniel <Deer.Daniel@stud.u-szeged.hu>
2021-11-26 14:52:14 +0100

    crypt

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -13,15 +13,16 @@ export class AuthService {
   constructor(private storage: Storage, private afs: AngularFirestore, private  router:  Router, public toastController: ToastController) { }
 
   toast: any;
-  salt = bcrypt.genSaltSync(10);
 
-  register(regemail: string, regpass: string, regname: string) {
-    let pass = bcrypt.hashSync(regpass, this.salt);
+  async register(regemail: string, regpass: string, regname: string) {
+    let salt = bcrypt.genSalt(10);
+    let pass = bcrypt.hash(regpass, await salt);
     const qres = {} as any;
     qres.authored = new Date().toISOString();
     qres.name = regname;
     qres.email = regemail;
-    qres.password = pass;
+    qres.password = await pass;
+    qres.salt = await salt;
     this.afs.collection('users').add(qres);
   }
 
@@ -49,12 +50,12 @@ export class AuthService {
 
   getUsers(logemail: string, logpass: string) {
     let siker = false;
-    let pass = bcrypt.hashSync(logpass, this.salt);
     let userDoc = this.afs.firestore.collection(`users`);
     userDoc.get().then((querySnapshot) => { 
       querySnapshot.forEach((doc) => {
         if (doc.data()) {
           let user = doc.data();
+          let pass = bcrypt.hashSync(logpass, user.salt);
           if (user.email === logemail && user.password === pass) {
             this.storage.set('user', user.email);
             this.router.navigateByUrl('home');
@@ -62,9 +63,10 @@ export class AuthService {
           }
         }
       })
-      if (!siker) this.wrongLogin();
+      if (!siker) {
+        this.wrongLogin();
+      } 
     })
-    
   }
 
 }
